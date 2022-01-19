500c7ebf6bf28f6a248e757dcad089b9d463010f
IlIAntonIlI <72148852+IlIAntonIlI@users.noreply.github.com>
2022-01-19 21:25:13 +0200

    Stopwatch
    * Project configuration
    
    * CI/CD
    
    * Delete prettier
    
    * Fix CI/CD, pre-commit
    
    * Skelet project
    
    * Start/Stop/Reset/Wait functions
    
    * Empty commit. Deploy vercel

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.scss'
+import StopWatch from './components/stopWatch/stopWatch'
+
+function App () {
+  return (
+    <div className="app">
+      <StopWatch />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/stopWatch/stopWatch.js
@@ -0,0 +1,63 @@
+import './stopWatch.scss'
+import { Observable } from 'rxjs'
+import { useState } from "react"
+
+let stream$ = new Observable();
+let subscription;
+let interval;
+let lastClick;
+
+function start(val, func){
+  if(subscription?.closed || !subscription){
+    let counter=val;
+    stream$ = new Observable(observer=>{
+      interval = setInterval(() => {
+        observer.next(++counter);
+      }, 1000);
+    })
+    subscription = stream$.subscribe(val =>{
+      func(val);
+    })
+  }
+}
+
+function stop(func){
+  subscription.unsubscribe();
+  func(0);
+}
+
+function reset(func){
+  func(0);
+  if (!subscription.closed) {
+    subscription.unsubscribe();
+    start(0,func);
+  }
+}
+
+function wait(){
+  if (lastClick){
+    Date.now()-lastClick<300 && subscription.unsubscribe();
+    lastClick = undefined;
+    return;
+  } 
+  lastClick = Date.now();
+}
+
+function stopWatch () {
+  const [time,setTime] = useState(0);
+  return (
+    <div className="stopwatch">
+      <div className="time-panel">
+        <div>{new Date(time * 1000).toISOString().substring(11, 19)}</div>
+      </div>
+      <div className="buttons-panel">
+        <button onClick={()=>{start(time,setTime)}}>Start</button>
+        <button onClick={()=>{stop(setTime)}}>Stop</button>
+        <button onClick={()=>{reset(setTime)}}>Reset</button>
+        <button onClick={wait}>Wait</button>
+      </div>
+    </div>
+  )
+}
+
+export default stopWatch
